493e9739a7029b14a4b75c1ed38e5ffbceee9110
luisra99 <luisraul.alfonsoc@gmail.com>
2024-10-15 22:40:47 -0400

    update

--- src/pages/Mesas/Mesas.old.tsx
+++ src/pages/Mesas/Mesas.old.tsx
@@ -7,8 +7,6 @@ import { useEffect, useState } from "react";
 import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
 import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
 import HomeIcon from "@mui/icons-material/Home";
-import { listTables } from "@/services/table";
-import axios from "axios";
 const style = {
   position: "absolute",
   top: "50%",
@@ -24,79 +22,63 @@ const style = {
 function Mesas() {
   const [open, setOpen] = useState(false);
   const [ammount, setAmmount] = useState<string>("");
-  const [mesas, setMesas] = useState<any[]>([]);
-  const Load = async () => {
-    const _concepts = await listTables();
-    setMesas([..._concepts]);
-  };
 
-  useEffect(() => {
-    Load();
-    axios.post(`http://localhost:4000/accounts`, {
-      name: "Raul",
-      description: "el informatico",
-      people: 4,
-      idDependent: null,
-      idTable: null,
-    });
-  }, []);
-
-  // const mesas: any = [
-  //   { name: "Mesa 1", orders: 1, amount: 56.87, status: "libre" },
-  //   {
-  //     name: "Mesa 2",
-  //     orders: 2,
-  //     amount: 56.87,
-  //     status: "ocupada",
-  //     personas: 2,
-  //   },
-  //   {
-  //     name: "Mesa 3",
-  //     orders: 2,
-  //     amount: 56.87,
-  //     status: "reservada",
-  //     personas: 4,
-  //   },
-  //   {
-  //     name: "Mesa 4",
-  //     orders: 2,
-  //     amount: 56.87,
-  //     status: "esperando_pedido",
-  //     personas: 2,
-  //   },
-  //   {
-  //     name: "Mesa 5",
-  //     orders: 2,
-  //     amount: 56.87,
-  //     status: "esperando_cuenta",
-  //     personas: 2,
-  //   },
-  //   { name: "Mesa 6", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 7", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 8", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 9", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 10", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 11", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 12", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 13", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 14", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 15", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 16", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 17", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 18", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 19", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 20", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 21", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 22", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 23", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 24", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 25", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 26", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 27", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 28", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 29", orders: 2, amount: 56.87 },
-  //   { name: "Mesa 30", orders: 2, amount: 56.87 },
-  // ];
+  const mesas: any = [
+    { name: "Mesa 1", orders: 1, amount: 56.87, status: "libre" },
+    {
+      name: "Mesa 2",
+      orders: 2,
+      amount: 56.87,
+      status: "ocupada",
+      personas: 2,
+    },
+    {
+      name: "Mesa 3",
+      orders: 2,
+      amount: 56.87,
+      status: "reservada",
+      personas: 4,
+    },
+    {
+      name: "Mesa 4",
+      orders: 2,
+      amount: 56.87,
+      status: "esperando_pedido",
+      personas: 2,
+    },
+    {
+      name: "Mesa 5",
+      orders: 2,
+      amount: 56.87,
+      status: "esperando_cuenta",
+      personas: 2,
+    },
+    { name: "Mesa 6", orders: 2, amount: 56.87 },
+    { name: "Mesa 7", orders: 2, amount: 56.87 },
+    { name: "Mesa 8", orders: 2, amount: 56.87 },
+    { name: "Mesa 9", orders: 2, amount: 56.87 },
+    { name: "Mesa 10", orders: 2, amount: 56.87 },
+    { name: "Mesa 11", orders: 2, amount: 56.87 },
+    { name: "Mesa 12", orders: 2, amount: 56.87 },
+    { name: "Mesa 13", orders: 2, amount: 56.87 },
+    { name: "Mesa 14", orders: 2, amount: 56.87 },
+    { name: "Mesa 15", orders: 2, amount: 56.87 },
+    { name: "Mesa 16", orders: 2, amount: 56.87 },
+    { name: "Mesa 17", orders: 2, amount: 56.87 },
+    { name: "Mesa 18", orders: 2, amount: 56.87 },
+    { name: "Mesa 19", orders: 2, amount: 56.87 },
+    { name: "Mesa 20", orders: 2, amount: 56.87 },
+    { name: "Mesa 21", orders: 2, amount: 56.87 },
+    { name: "Mesa 22", orders: 2, amount: 56.87 },
+    { name: "Mesa 23", orders: 2, amount: 56.87 },
+    { name: "Mesa 24", orders: 2, amount: 56.87 },
+    { name: "Mesa 25", orders: 2, amount: 56.87 },
+    { name: "Mesa 26", orders: 2, amount: 56.87 },
+    { name: "Mesa 27", orders: 2, amount: 56.87 },
+    { name: "Mesa 28", orders: 2, amount: 56.87 },
+    { name: "Mesa 29", orders: 2, amount: 56.87 },
+    { name: "Mesa 30", orders: 2, amount: 56.87 },
+  ];
   return (
     <>
       <Meta title="Configuración" />
@@ -110,15 +92,15 @@ function Mesas() {
                   padding: 2,
                   borderRadius: "8px",
                   backgroundColor:
-                    mesa.status === 0
+                    mesa.status === "libre"
                       ? "lightgreen"
-                      : mesa.status === 1
+                      : mesa.status === "ocupada"
                         ? "lightcoral"
-                        : mesa.status === 2
+                        : mesa.status === "reservada"
                           ? "lightyellow"
-                          : mesa.status === 3
+                          : mesa.status === "esperando_pedido"
                             ? "orange"
-                            : mesa.status === 4
+                            : mesa.status === "esperando_cuenta"
                               ? "lightblue"
                               : "lightgray",
                   cursor: "pointer",
@@ -134,25 +116,17 @@ function Mesas() {
                 >
                   {/* Nombre de la mesa */}
                   <Typography variant="h6">{mesa.name}</Typography>
-
                   {/* Cantidad de personas */}
-                  <Box display="flex" alignItems="center">
-                    <Person fontSize="small" sx={{ marginRight: 0.5 }} />
-                    <Typography variant="body2" color="textSecondary">
-                      {mesa.Account && `${mesa.Account.people ?? "¿?"}/`}
-                      {mesa.capacity}
-                    </Typography>
-                  </Box>
+                  {!!mesa.personas && (
+                    <Box display="flex" alignItems="center">
+                      <Person fontSize="small" sx={{ marginRight: 0.5 }} />
+                      <Typography variant="body2" color="textSecondary">
+                        {mesa.personas}
+                      </Typography>
+                    </Box>
+                  )}
                 </Box>
 
-                {/* Muestra los detalles de la mesa si no hay una cuenta abierta */}
-                {!mesa.Account && (
-                  <Box alignItems="left">
-                    <Typography variant="body2" color="textSecondary">
-                      {mesa.details}
-                    </Typography>
-                  </Box>
-                )}
                 {/* Monto total y tiempo de ocupación */}
                 <Box
                   display="flex"
@@ -161,40 +135,34 @@ function Mesas() {
                   sx={{ marginTop: 0.5 }}
                 >
                   {/* Monto total de la cuenta */}
-                  {mesa.Account && (
-                    <Typography variant="caption" color="textSecondary">
-                      ${mesa.amount}
-                    </Typography>
-                  )}
+                  <Typography variant="caption" color="textSecondary">
+                    ${mesa.amount}
+                  </Typography>
 
                   {/* Tiempo de ocupación */}
-                  {mesa.Account && (
+                  {mesa.status !== "libre" && (
                     <Box display="flex" alignItems="center">
                       <Timelapse fontSize="small" sx={{ marginRight: 0.5 }} />
                       <Typography variant="caption" color="textSecondary">
-                        {mesa.Account.tiempoOcupacion} minutos
+                        {mesa.tiempoOcupacion} minutos
                       </Typography>
                     </Box>
                   )}
                 </Box>
 
                 {/* Productos en la mesa */}
-                {mesa.Account && (
-                  <Typography variant="body1" sx={{ marginTop: 1 }}>
-                    {mesa.Account._count.details}{" "}
-                    {mesa.Account._count.details > 1 ? "productos" : "producto"}
-                  </Typography>
-                )}
+                <Typography variant="body1" sx={{ marginTop: 1 }}>
+                  {mesa.orders} {mesa.orders > 1 ? "productos" : "producto"}
+                </Typography>
+
                 {/* Mesero asignado */}
-                {mesa.Account && (
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    sx={{ marginTop: 0.5 }}
-                  >
-                    Mesero: {mesa.Account.dependent || "No asignado"}
-                  </Typography>
-                )}
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  sx={{ marginTop: 0.5 }}
+                >
+                  Mesero: {mesa.mesero || "No asignado"}
+                </Typography>
               </Paper>
             </Grid>
           ))}
